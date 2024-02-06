0c37b83fa21ee9a3b24129de5b20a133ba4cb6b8
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-02-07 00:29:53 +0530

    create endpoints

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (_, res) => {
+  res.status(200).json({
+    projectName: "Techlog.tech",
+    description: "A Blogging Website",
+  });
+});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀🚀 Server Running... on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`DB - Connection Error`, error);
+  });
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -10,7 +10,7 @@ const router = Router();
 
 router.route("/create").post(createBlog);
 router.route("/blogs").get(getBlogs);
-router.route("/update").post(updateBlog);
-router.route("/delete").post(deteleBlog);
+router.route("/update").put(updateBlog);
+router.route("/delete").delete(deteleBlog);
 
 export default router;
