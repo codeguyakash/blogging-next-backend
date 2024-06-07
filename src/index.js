3ed0e8c49b6c9dad47e83fdb62e81aca5cbcfc20
Akash <codeguyakash@gmail.com>
2024-06-07 15:04:38 +0530

    add title and desc

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { app } from "./app.js";
 import connectDB from "./db/db.js";
 
 const PORT = process.env.PORT || 5000;
-app.get("/", (_, res) => {
+app.get("/api/v1/name", (_, res) => {
   res.status(200).json({
-    projectName: "techlogs.tech",
-    description: "A Blogging Website",
+    projectName: "Techlogs.tech: Your Gateway to Tech Insights",
+    description:
+      "Techlogs simplifies tech blogging, offering a streamlined platform to share and explore the latest in technology. Dive into insightful articles, connect with fellow enthusiasts, and stay ahead of the curve. Your tech journey starts here",
   });
 });
 
